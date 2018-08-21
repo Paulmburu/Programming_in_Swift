fb7448b42ab7b1f71dd30e40e24cb8c3d6b8f6f2
Paul Mburu <paulmburu53@gmail.com>
2018-08-21 22:44:46 +0300

    swift for loops

---
+++ forLoops.swift
@@ -0,0 +1,12 @@
+ // half closed range operator
+
+ for index in 1..<5{
+     print(index)       // 1 2 3 4
+ }
+
+ // closed-RAnge operator
+ // defines a range that includes the last number
+
+ for index in 1...5{
+     print(index)
+ }
