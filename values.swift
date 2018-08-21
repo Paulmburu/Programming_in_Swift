60892ac65af6a7955e6eebef90fbe2fcc5f9c2aa
Paul Mburu <paulmburu53@gmail.com>
2018-08-21 22:37:10 +0300

    swift values

---
+++ values.swift
@@ -0,0 +1,15 @@
+// adding strings (concatenation)
+print("swift" + "lang") //swift lang 
+print(1+1)		// 2
+
+// type casting
+print("1+1= "+String(1+1))  // 1+1=2
+print(7.0 / 3.0) 	    // 2.3333333333
+print(String(7.0)+" / " + String(3.0)) // 7.0 / 3.0
+
+
+//booleans
+print(true && false)
+print(true || false)
+print(!true)
+
